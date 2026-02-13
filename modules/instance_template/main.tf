d323f2c048b94ffd2375c8159c41eb4fad1da97d
Vandna Garg <vandnagarg@google.com>
2026-02-13 06:38:12 +0000

    remove parent nic

--- modules/instance_template/main.tf
+++ modules/instance_template/main.tf
@@ -197,7 +197,6 @@ resource "google_compute_instance_template" "tpl" {
       stack_type         = network_interface.value.stack_type
       network_attachment = network_interface.value.network_attachment
       vlan               = network_interface.value.vlan
-      parent_nic_name  = network_interface.value.parent_nic_name 
       queue_count        = network_interface.value.queue_count
       dynamic "access_config" {
         for_each = network_interface.value.access_config
