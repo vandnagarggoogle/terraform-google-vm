e6fb884bb35e5e7da13cf6d001cee3b5114db9eb
Vandna Garg <vandnagarg@google.com>
2026-03-01 14:35:48 +0000

    chore: adding mig test

--- test/integration/mig_simple/controls/mig_simple.rb
+++ test/integration/mig_simple/controls/mig_simple.rb
@@ -1,16 +1,5 @@
 # Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# ... (standard header) ...
 
 project_id = attribute('project_id')
 region     = attribute('region')
@@ -21,7 +10,28 @@ expected_instance_groups = 1
 control "MIG" do
   title "Simple Configuration"
 
-  describe command("gcloud --project=#{project_id} compute instances list --format=json --filter='name~^mig-simple*'") do
+  # 1. Add a wait loop to handle Compute API eventual consistency.
+  # Regional queries are strongly consistent, but a 30s buffer is safest for CI.
+  describe "GCE API Propagation" do
+    it "should find resources within 60 seconds" do
+      success = false
+      6.times do |i|
+        # Use '--filter=name:...' (substring) which is more robust than regex in gcloud
+        # IMPORTANT: Added '--regions' flag to hit the strongly consistent regional endpoint
+        cmd = command("gcloud compute instances list --project=#{project_id} --regions=#{region} --format=json --filter='name:mig-simple'")
+        if cmd.exit_status == 0 && JSON.parse(cmd.stdout).length == expected_instances
+          success = true
+          break
+        end
+        puts "Attempt #{i+1}/6: Instances not visible yet. Sleeping 10s..."
+        sleep 10
+      end
+      expect(success).to be true
+    end
+  end
+
+  # 2. Update Instance Verification to use the Regional endpoint
+  describe command("gcloud --project=#{project_id} compute instances list --regions=#{region} --format=json --filter='name:mig-simple'") do
     its(:exit_status) { should eq 0 }
     its(:stderr) { should eq '' }
 
@@ -40,7 +50,8 @@ control "MIG" do
     end
   end
 
-  describe command("gcloud --project=#{project_id} compute instance-groups list --format=json --filter='name~^mig-simple*'") do
+  # 3. Update Instance Group Verification to use the Regional endpoint
+  describe command("gcloud --project=#{project_id} compute instance-groups list --regions=#{region} --format=json --filter='name:mig-simple'") do
     its(:exit_status) { should eq 0 }
     its(:stderr) { should eq '' }
 
@@ -59,7 +70,8 @@ control "MIG" do
     end
   end
 
-  describe command("gcloud --project=#{project_id} compute instance-groups managed list --format=json --filter='name~^mig-simple*'") do
+  # 4. Update Managed Instance Group Verification to use the Regional endpoint
+  describe command("gcloud --project=#{project_id} compute instance-groups managed list --regions=#{region} --format=json --filter='name:mig-simple'") do
     its(:exit_status) { should eq 0 }
     its(:stderr) { should eq '' }
 
@@ -71,11 +83,10 @@ control "MIG" do
       end
     end
 
-    describe "number of instance groups" do
+    describe "number of managed instance groups" do
       it "should be #{expected_instance_groups}" do
         expect(data.length).to eq(expected_instance_groups)
       end
     end
   end
-
 end
